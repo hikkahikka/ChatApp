97649a532d74e788970c3f89598b862c28fa062c
Boris <mr_boriska1998@mail.ru>
2026-07-24 15:36:52 +0300

    Fixed pointer color

--- ConsoleChatClient/ConsoleChatClient/ConsoleUI.cs
+++ ConsoleChatClient/ConsoleChatClient/ConsoleUI.cs
@@ -6,42 +6,31 @@ namespace ConsoleChatClient
 {
     public class ConsoleUI
     {
+
         public void DisplayMessage(string sender, string message, string currentUserName)
         {
             var defaultColor = Console.ForegroundColor;
-
+            Console.ForegroundColor= GetConsoleColor(sender, currentUserName);
+            Console.Write("> ");
             if (sender == currentUserName)
             {
-                Console.ForegroundColor = ConsoleColor.Magenta;
                 Console.WriteLine($"[You]: {message}");
-            }
-            else if (sender == "System")
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine($"[System]: {message}");
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine($"[{sender}]: {message}");
             }
-            Console.Write("> ");
             Console.ForegroundColor = defaultColor;
         }
         public void ClearLastInputLine()
         {
             Console.SetCursorPosition(0, Console.CursorTop - 1);
-
             Console.Write(new string(' ', Console.WindowWidth));
-
             Console.SetCursorPosition(0, Console.CursorTop);
         }
         public void DisplayInfo(string message)
         {
-            var defaultColor = Console.ForegroundColor;
-            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine(message);
-            Console.ForegroundColor = defaultColor;
         }
         public string AskUserName()
         {
@@ -54,6 +43,21 @@ namespace ConsoleChatClient
             Console.Write("Enter chat room name (or \"/exit\" to exit): ");
             string chatRoom = Console.ReadLine() ?? "General";
             return chatRoom;
+        }
+        private ConsoleColor GetConsoleColor(string sender, string currentUserName)
+        {
+            if (sender == currentUserName)
+            {
+                return ConsoleColor.Magenta;
+            }
+            else if (sender == "System")
+            {
+                return ConsoleColor.Yellow;
+            }
+            else
+            {
+                return ConsoleColor.Green;
+            }
         }
     }
 }
